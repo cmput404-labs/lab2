a695baf4bad4557e82010f09743cdfe215df56b8
KevinHQi <35250630+KevinHQi@users.noreply.github.com>
2021-01-20 20:14:18 -0700

    Update client.py

--- client.py
+++ client.py
@@ -37,8 +37,8 @@ def send_data(serversocket, payload):
 def main():
     try:
         #define address info, payload, and buffer size
-        host = ''
-        port = 8001
+        host = 'www.google.com'
+        port = 80
         payload = f'GET / HTTP/1.0\r\nHost: {host}\r\n\r\n'
         buffer_size = 4096
 
